bd52fb704e49dbd4a88c145864494457203951fa
y0956880-Eric <y0956880@gmail.com>
2025-11-22 16:33:31 +0800

    1141122提交

---
+++ Lesson4/Lesson4_3.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright
+import os
+from time import sleep
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=500)        
+        page = browser.new_page()
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        html_file = os.path.join(current_dir,"form_demo.html")
+        page.goto(f"file://{html_file}")
+        sleep(3)
+        browser.close()
+      
+
+if __name__ =="__main__":
+    main()
